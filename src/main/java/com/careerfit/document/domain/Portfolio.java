63bc8f042bf8fce507cd3c94eb65de5d6c94ff51
lwj0831 <wjl0831@gmail.com>
2025-08-27 19:37:15 +0900

    feat(docuemnt): Portfolio 엔티티 구현

---
+++ src/main/java/com/careerfit/document/domain/Portfolio.java
@@ -0,0 +1,33 @@
+package com.careerfit.document.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "portfolio")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String s3Key;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "document_id")
+    private Document document;
+
+}
