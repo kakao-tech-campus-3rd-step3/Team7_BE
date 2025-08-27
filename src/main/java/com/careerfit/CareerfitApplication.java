ecf25f3e9bbfceb5d9e40505e6d03d34399ce2d1
lwj0831 <wjl0831@gmail.com>
2025-08-27 18:48:28 +0900

    feat(global): 공통 JpaAuditing 엔티티 구현

--- src/main/java/com/careerfit/CareerfitApplication.java
+++ src/main/java/com/careerfit/CareerfitApplication.java
@@ -2,7 +2,9 @@ package com.careerfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CareerfitApplication {
 
