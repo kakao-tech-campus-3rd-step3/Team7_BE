7bdde0e2fe7a548835f54463cac3dd6d0d6803e4
lwj0831 <wjl0831@gmail.com>
2025-08-28 15:39:14 +0900

    feat(auth): 카카오 API 예외처리 핸들러 구현

---
+++ src/main/java/com/careerfit/auth/exception/AuthExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.careerfit.auth.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.careerfit.global.dto.ApiResponse;
+import com.careerfit.global.exception.ErrorCode;
+import com.careerfit.global.exception.ErrorInfo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestControllerAdvice
+@Slf4j
+public class AuthExceptionHandler {
+
+    @ExceptionHandler({KakaoApiClientException.class, KakaoApiServerException.class})
+    public ResponseEntity<ApiResponse<ErrorInfo>> handleKakaoApiException(
+        KakaoApiClientException exception) {
+        log.error("카카오 API 에러 발생 : {}", exception.getMessage());
+
+        ErrorCode errorCode = exception.getErrorCode();
+
+        return ResponseEntity.status(errorCode.getStatus())
+            .body(ApiResponse.error(errorCode.getCode(), errorCode.getMessage(),
+                ErrorInfo.of(exception.getErrorInfo())));
+
+    }
+}
