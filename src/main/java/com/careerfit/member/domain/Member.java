05fc9c5d2dccb5948d5f06b92bd83bede4f04783
lwj0831 <wjl0831@gmail.com>
2025-08-29 15:11:41 +0900

    chore: OAuthProvider 패키지 이동 및 속성 추가

--- src/main/java/com/careerfit/member/domain/Member.java
+++ src/main/java/com/careerfit/member/domain/Member.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import com.careerfit.application.domain.Application;
 import com.careerfit.global.entity.TimeBaseEntity;
-import com.careerfit.login.domain.OAuthProviderType;
+import com.careerfit.auth.domain.OAuthProvider;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -17,12 +17,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 @Getter
 public class Member extends TimeBaseEntity {
 
@@ -40,16 +44,39 @@ public class Member extends TimeBaseEntity {
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private OAuthProviderType oAuthProviderType;
+    private OAuthProvider oAuthProvider;
 
     @Column(nullable = false)
-    private Long oauthId;
+    private String oauthId;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private MemberRole memberRole;
 
+    @Builder.Default
     @OneToMany(mappedBy = "member")
     private List<Application> applications = new ArrayList<>();
+
+    public static Member mento(String email, String phoneNumber, String profileImageUrl, OAuthProvider oAuthProvider, String oauthId) {
+        return Member.builder()
+            .email(email)
+            .phoneNumber(phoneNumber)
+            .profileImageUrl(profileImageUrl)
+            .oAuthProvider(oAuthProvider)
+            .oauthId(oauthId)
+            .memberRole(MemberRole.MENTO)
+            .build();
+    }
+
+    public static Member mentee(String email, String phoneNumber, String profileImageUrl, OAuthProvider oAuthProvider, String oauthId) {
+        return Member.builder()
+            .email(email)
+            .phoneNumber(phoneNumber)
+            .profileImageUrl(profileImageUrl)
+            .oAuthProvider(oAuthProvider)
+            .oauthId(oauthId)
+            .memberRole(MemberRole.MENTEE)
+            .build();
+    }
 
 }
