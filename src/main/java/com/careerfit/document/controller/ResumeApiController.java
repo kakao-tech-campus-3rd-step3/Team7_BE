888cea90b66ae38434996e19a03de695943bb465
Hyun <hyun_7586@naver.com>
2025-09-04 01:12:21 +0900

    feat: ResumeApiController 생성
    - PresignedUrl 발급 요청, 파일 업로드 완료 처리 API 구현

--- src/main/java/com/careerfit/document/controller/ResumeApiController.java
+++ src/main/java/com/careerfit/document/controller/ResumeApiController.java
@@ -1,12 +1,13 @@
 package com.careerfit.document.controller;
 
-import com.careerfit.document.domain.DocumentType;
+import com.careerfit.document.dto.CompleteUploadRequest;
 import com.careerfit.document.dto.PresignedUrlRequest;
 import com.careerfit.document.dto.PresignedUrlResponse;
-import com.careerfit.document.service.FileUploadService;
+import com.careerfit.document.dto.ResumeCreateResponse;
+import com.careerfit.document.service.ResumeService;
 import com.careerfit.global.dto.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,22 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/resumes")
+@RequestMapping("/api/applications/{application-id}")
 public class ResumeApiController {
 
-    private final FileUploadService fileUploadService;
+    private final ResumeService resumeService;
 
-    @PostMapping("/{user-id}")
+    // 파일 업로드 요청: PresignedUrl 발급.
+    @PostMapping("/resume")
     public ResponseEntity<ApiResponse<PresignedUrlResponse>> generatePresignedUrl(
-        @PathVariable(name = "user-id") Long userId,
-        @RequestBody PresignedUrlRequest request) {
-        PresignedUrlResponse response = fileUploadService.generatePresignedUrl(
-            userId,
-            request.fileName(),
-            request.fileType(),
-            DocumentType.RESUME
-        );
-        return ResponseEntity.status(HttpStatus.OK)
-            .body(ApiResponse.success(response));
+        @PathVariable(name = "application-id") Long applicationId,
+        @Valid @RequestBody PresignedUrlRequest request) {
+
+        PresignedUrlResponse response = resumeService.generatePresignedUrl(applicationId, request);
+        return ResponseEntity.ok(ApiResponse.success(response));
     }
+
+    // 파일 업로드 완료 처리: 서버에 업로드 완료 여부를 전달하는 API, 여기서 파일 메타데이터를 DB에 저장.
+    @PostMapping("/resume/complete-upload")
+    public ResponseEntity<ApiResponse<?>> completeUploadResume(
+        @PathVariable(name = "application-id") Long applicationId,
+        @RequestBody CompleteUploadRequest request
+    ) {
+        ResumeCreateResponse response = resumeService.completeUploadFile(applicationId, request);
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
 }
