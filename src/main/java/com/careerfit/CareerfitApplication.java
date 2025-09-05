6ea8a9ce22085da61fcc3a412044902f37a8645d
Hyun <hyun_7586@naver.com>
2025-09-05 20:58:11 +0900

    refactor: @ConfigurationProperties 객체를 스캔하는 어노테이션 추가
    - CareerfitApplication에 @ConfigurationProperties를 스캔하는 어노테이션 추가

--- src/main/java/com/careerfit/CareerfitApplication.java
+++ src/main/java/com/careerfit/CareerfitApplication.java
@@ -2,10 +2,12 @@ package com.careerfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CareerfitApplication {
 
 	public static void main(String[] args) {
