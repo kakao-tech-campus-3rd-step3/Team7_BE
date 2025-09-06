1ff72d56fe6618133b22609d3db3e3893e7a1e9a
Hyun <hyun_7586@naver.com>
2025-09-06 13:18:16 +0900

    feat: 이력서/포트폴리오 조회/삭제를 담당하는 Service 코드 생성
    - ResumeService 생성
    - PortfolioService 생성

--- src/main/java/com/careerfit/document/service/ResumeService.java
+++ src/main/java/com/careerfit/document/service/ResumeService.java
@@ -1,9 +1,38 @@
 package com.careerfit.document.service;
 
+import com.careerfit.document.domain.Resume;
+import com.careerfit.document.dto.FileInfoResponse;
+import com.careerfit.document.exception.FileException;
+import com.careerfit.document.exception.ResumeErrorCode;
+import com.careerfit.document.repository.ResumeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ResumeService {
+
+    private final ResumeFinder resumeFinder;
+    private final ResumeRepository resumeRepository;
+
+    public FileInfoResponse getResumeInfo(Long applicationId, Long resumeId){
+        Resume resume = resumeFinder.findResumeOrThrow(resumeId);
+
+        if(!resume.getApplication().getId().equals(applicationId)){
+            throw new FileException(ResumeErrorCode.RESUME_NOT_MATCHED);
+        }
+
+        return FileInfoResponse.fromResume(resume);
+    }
+
+    public void deleteResume(Long applicationId, Long resumeId){
+        Resume resume = resumeFinder.findResumeOrThrow(resumeId);
+
+        if(!resume.getApplication().getId().equals(applicationId)){
+            throw new FileException(ResumeErrorCode.RESUME_NOT_MATCHED);
+        }
+
+        resumeRepository.deleteById(resumeId);
+    }
+
 }
