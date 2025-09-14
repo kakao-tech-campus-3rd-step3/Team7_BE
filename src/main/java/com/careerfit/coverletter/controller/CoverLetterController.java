f4e36854f71e3f2f262d221717c93c7187797010
lwj0831 <wjl0831@gmail.com>
2025-09-14 16:42:34 +0900

    chore: 자소서 Controller 명세 추가

--- src/main/java/com/careerfit/coverletter/controller/CoverLetterController.java
+++ src/main/java/com/careerfit/coverletter/controller/CoverLetterController.java
@@ -1,48 +1,60 @@
 package com.careerfit.coverletter.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.careerfit.coverletter.dto.CoverLetterDetailResponse;
 import com.careerfit.coverletter.dto.CoverLetterListResponse;
 import com.careerfit.coverletter.dto.CoverLetterRegisterRequest;
 import com.careerfit.coverletter.service.CoverLetterCommandService;
 import com.careerfit.coverletter.service.CoverLetterQueryService;
 import com.careerfit.global.dto.ApiResponse;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/applications/{applicationId}/cover-letters")
 @RequiredArgsConstructor
-public class CoverLetterController {
+public class CoverLetterController implements CoverLetterControllerDocs {
 
     private final CoverLetterCommandService coverLetterCommandService;
     private final CoverLetterQueryService coverLetterQueryService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<Void>> registerCoverLetter(@PathVariable(name = "applicationId") Long applicationId,
-                                                                 @Valid @RequestBody CoverLetterRegisterRequest dto) {
+    public ResponseEntity<ApiResponse<Void>> registerCoverLetter(
+        @PathVariable(name = "applicationId") Long applicationId,
+        @Valid @RequestBody CoverLetterRegisterRequest dto) {
         coverLetterCommandService.registerCoverLetter(applicationId, dto);
         return ResponseEntity.ok(ApiResponse.success());
     }
 
     @DeleteMapping("{documentId}")
-    public ResponseEntity<ApiResponse<Void>> deleteCoverLetter(@PathVariable(name = "documentId") Long documentId) {
+    public ResponseEntity<ApiResponse<Void>> deleteCoverLetter(
+        @PathVariable(name = "documentId") Long documentId) {
         coverLetterCommandService.deleteCoverLetter(documentId);
         return ResponseEntity.ok(ApiResponse.success());
     }
 
     @GetMapping("{documentId}")
-    public ResponseEntity<ApiResponse<CoverLetterDetailResponse>> getCoverLetterDetail(@PathVariable(name = "documentId") Long documentId) {
+    public ResponseEntity<ApiResponse<CoverLetterDetailResponse>> getCoverLetterDetail(
+        @PathVariable(name = "documentId") Long documentId) {
         CoverLetterDetailResponse coverLetterDetail = coverLetterQueryService.getCoverLetterDetail(
-                documentId);
+            documentId);
         return ResponseEntity.ok(ApiResponse.success(coverLetterDetail));
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<CoverLetterListResponse>> getCoverLetterLit(@PathVariable(name = "applicationId") Long applicationId) {
+    public ResponseEntity<ApiResponse<CoverLetterListResponse>> getCoverLetterLit(
+        @PathVariable(name = "applicationId") Long applicationId) {
         CoverLetterListResponse coverLetterList = coverLetterQueryService.getCoverLetterList(
-                applicationId);
+            applicationId);
         return ResponseEntity.ok(ApiResponse.success(coverLetterList));
     }
 
