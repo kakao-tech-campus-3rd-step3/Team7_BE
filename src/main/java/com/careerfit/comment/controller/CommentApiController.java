6268267f9d29d34a067c223fc6430c651dcb8b8c
Hyun <hyun_7586@naver.com>
2025-09-16 06:40:38 +0900

    feat: CommentApiController 생성 및 comment 생성 엔드포인트 추가
    - comment 생성 API createComment() 추가

---
+++ src/main/java/com/careerfit/comment/controller/CommentApiController.java
@@ -0,0 +1,44 @@
+package com.careerfit.comment.controller;
+
+import com.careerfit.comment.dto.CommentCreateRequest;
+import com.careerfit.comment.service.CommentService;
+import com.careerfit.global.dto.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/documents/{documentId}/comments")
+public class CommentApiController {
+
+    private final CommentService commentService;
+
+    // comment 생성
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> createComment(
+        @PathVariable Long documentId,
+        // 로그인 적용 시 @AuthenticationPrincipal로 변경 예정
+        @RequestParam Long memberId,
+        @RequestBody CommentCreateRequest request
+    ) {
+        commentService.createComment(documentId, memberId, request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ApiResponse.success());
+    }
+
+    // comment 단건 조회
+
+    // comment 리스트 조회
+
+    // comment 수정
+
+    // comment 삭제
+
+}
