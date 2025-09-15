e4ad02815e63f8eaac57b57e426b706b5f6643b7
Hyun <hyun_7586@naver.com>
2025-09-16 06:39:44 +0900

    feat: CommentService 생성 및 comment 생성 메서드 추가
    - createComment() 메서드 추가

---
+++ src/main/java/com/careerfit/comment/service/CommentService.java
@@ -0,0 +1,36 @@
+package com.careerfit.comment.service;
+
+import com.careerfit.comment.domain.Comment;
+import com.careerfit.comment.dto.CommentCreateRequest;
+import com.careerfit.comment.repository.CommentRepository;
+import com.careerfit.document.domain.Document;
+import com.careerfit.document.service.DocumentFinder;
+import com.careerfit.member.domain.Member;
+import com.careerfit.member.service.MemberFinder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final DocumentFinder documentFinder;
+    private final MemberFinder memberFinder;
+
+    // comment 생성
+    @Transactional
+    public Comment createComment(
+        Long documentId,
+        Long memberId,
+        CommentCreateRequest request
+    ) {
+        Document document = documentFinder.findDocumentOrThrow(documentId);
+        Member member = memberFinder.getMemberOrThrow(memberId);
+        Comment comment = Comment.of(document, member, request);
+
+        return commentRepository.save(comment);
+    }
+
+}
