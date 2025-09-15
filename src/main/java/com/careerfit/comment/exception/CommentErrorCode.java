2ba5703b53a5b178b0426fb4dd93e5bf6443f6c4
Hyun <hyun_7586@naver.com>
2025-09-16 06:56:45 +0900

    feat: CommentErrorCode 생성 및 COMMENT_NOT_FOUND 추가

---
+++ src/main/java/com/careerfit/comment/exception/CommentErrorCode.java
@@ -0,0 +1,16 @@
+package com.careerfit.comment.exception;
+
+import com.careerfit.global.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommentErrorCode implements ErrorCode {
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT-001", "코멘트가 존재하지 않습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
