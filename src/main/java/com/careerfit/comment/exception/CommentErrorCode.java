981ed5b51357874552570c83045750959482f088
Hyun <hyun_7586@naver.com>
2025-09-16 08:46:02 +0900

    refactor: CommentErrorCode에 COMMENT_NOT_ALLOWED 추가

--- src/main/java/com/careerfit/comment/exception/CommentErrorCode.java
+++ src/main/java/com/careerfit/comment/exception/CommentErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CommentErrorCode implements ErrorCode {
-    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT-001", "코멘트가 존재하지 않습니다.");
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT-001", "코멘트가 존재하지 않습니다."),
+    COMMENT_NOT_ALLOWED(HttpStatus.FORBIDDEN, "COMMENT-002", "코멘트를 조회할 권한이 없습니다.");
 
     private final HttpStatus status;
     private final String code;
