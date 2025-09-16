93e0197460d88f71f7163871f842ba7e20506d17
Hyun <hyun_7586@naver.com>
2025-09-16 17:23:03 +0900

    refactor: CommentErrorCode에 NOT_ALLOWED_TO_QUERY, NOT_ALLOWED_TO_COMMAND 추가

--- src/main/java/com/careerfit/comment/exception/CommentErrorCode.java
+++ src/main/java/com/careerfit/comment/exception/CommentErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CommentErrorCode implements ErrorCode {
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT-001", "코멘트가 존재하지 않습니다."),
-    COMMENT_NOT_ALLOWED(HttpStatus.FORBIDDEN, "COMMENT-002", "코멘트를 조회할 권한이 없습니다.");
+    NOT_ALLOWED_TO_QUERY(HttpStatus.FORBIDDEN, "COMMENT-002", "코멘트를 조회할 권한이 없습니다."),
+    NOT_ALLOWED_TO_COMMAND(HttpStatus.FORBIDDEN, "COMMENT-003", "코멘트를 수정/삭제할 권한이 없습니다.");
 
     private final HttpStatus status;
     private final String code;
