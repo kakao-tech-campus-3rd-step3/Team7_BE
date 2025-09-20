4739f164a0f25b7e422c6969ccf65558d899c03e
Hyun <hyun_7586@naver.com>
2025-09-21 00:29:07 +0900

    feat: AttachmentFile 메타 데이터 접근 전용 service 객체 2개 생성
    - CQRS 패턴을 고려해 CommandService와 QueryService로 분리

---
+++ src/main/java/com/careerfit/attachmentfile/service/AttachmentFileCommandService.java
@@ -0,0 +1,49 @@
+package com.careerfit.attachmentfile.service;
+
+import com.careerfit.application.exception.ApplicationErrorCode;
+import com.careerfit.application.service.ApplicationFinder;
+import com.careerfit.attachmentfile.domain.AttachmentFile;
+import com.careerfit.attachmentfile.repository.AttachmentFileRepository;
+import com.careerfit.document.domain.DocumentType;
+import com.careerfit.global.exception.ApplicationException;
+import com.careerfit.global.util.DocumentUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AttachmentFileCommandService {
+
+    private final AttachmentFileFinder attachmentFileFinder;
+    private final AttachmentFileRepository attachmentFileRepository;
+    private final ApplicationFinder applicationFinder;
+
+    // 파일 메타 데이터 저장
+    public void saveFile(Long requestApplicationId, String uniqueFileName, DocumentType documentType) {
+
+        Long applicationId = DocumentUtil.extractApplicationId(uniqueFileName);
+        String documentTitle = DocumentUtil.extractDocumentTitle(uniqueFileName);
+        String originalFileName = DocumentUtil.extractOriginalFileName(uniqueFileName);
+
+        // /api/application/{requestApplicationId}/~와 presignedUrl에 담긴 applicationId가 다르면 예외 발생
+        if (!applicationId.equals(requestApplicationId)) {
+            throw new ApplicationException(ApplicationErrorCode.APPLICATION_UNMATCHED);
+        }
+
+        AttachmentFile attachmentFile = AttachmentFile.of(originalFileName, uniqueFileName,
+            documentTitle, applicationFinder.getApplicationOrThrow(applicationId), documentType);
+
+        attachmentFileRepository.save(attachmentFile);
+    }
+
+    // 파일 메타 데이터 삭제
+    public void deleteFile(Long applicationId, Long attachmentFileId) {
+        AttachmentFile attachmentFile = attachmentFileFinder.findAttachmentFileOrThrow(
+            attachmentFileId);
+        AttachmentFileVerifier.verifyApplicationOwnership(applicationId, attachmentFile);
+
+        attachmentFileRepository.deleteById(attachmentFileId);
+    }
+}
