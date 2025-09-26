86583eaffcdfbce5316cb7390c4d593d31fdc202
Hyun <hyun_7586@naver.com>
2025-09-26 15:02:50 +0900

    refactor: Request dto 2종에 존재하는 String 필드에 @NotBlank() 제약조건 추가
    - CommentCreateRequest String 필드값에 @NotBlank 추가
    - FileUploadRequest String 필드값에 @NotBlank 추가

--- src/main/java/com/careerfit/attachmentfile/dto/FileUploadRequest.java
+++ src/main/java/com/careerfit/attachmentfile/dto/FileUploadRequest.java
@@ -1,12 +1,17 @@
 package com.careerfit.attachmentfile.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record FileUploadRequest(
         @Pattern(regexp = "^[^_]*$", message = "문서 제목에는 '_'를 사용할 수 없습니다.")
+        @NotBlank(message = "문서명을 입력해 주세요.")
         String documentTitle,
 
         @Pattern(regexp = "^[^_]*$", message = "파일 이름에는 '_'를 사용할 수 없습니다.")
+        @NotBlank(message = "파일명을 입력해 주세요.")
         String fileName,
+
+        @NotBlank(message = "파일타입을 입력해 주세요.")
         String fileType
 ) {}
--- src/main/java/com/careerfit/comment/dto/CommentCreateRequest.java
+++ src/main/java/com/careerfit/comment/dto/CommentCreateRequest.java
@@ -1,8 +1,10 @@
 package com.careerfit.comment.dto;
 
 import com.careerfit.comment.domain.Coordinate;
+import jakarta.validation.constraints.NotBlank;
 
 public record CommentCreateRequest(
+    @NotBlank(message = "댓글 내용을 입력해 주세요.")
     String content,
     Coordinate coordinate
 ) {}
