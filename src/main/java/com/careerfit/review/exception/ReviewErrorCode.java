48b8508025ed6cb42d52eccedb0ac08368a37a03
박신영 <park_new0@naver.com>
2025-09-27 01:18:49 +0900

    feat(review): 자기 자신 리뷰 방지용 에러 코드 추가 및 방어로직 추가

--- src/main/java/com/careerfit/review/exception/ReviewErrorCode.java
+++ src/main/java/com/careerfit/review/exception/ReviewErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum ReviewErrorCode implements ErrorCode {
 
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "REVIEW-001", "해당 리뷰를 찾을 수 없습니다."),
-    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "REVIEW-002", "해당 리뷰에 대한 접근 권한이 없습니다.");
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "REVIEW-002", "해당 리뷰에 대한 접근 권한이 없습니다."),
+    CANNOT_REVIEW_SELF(HttpStatus.BAD_REQUEST, "REVIEW-003", "자기 자신에게 리뷰를 작성할 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
