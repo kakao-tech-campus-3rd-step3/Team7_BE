c3fb903a1bade3b182823b34cde16343bb3d1f88
HyunJunCho00 <catlove5478@naver.com>
2025-09-26 16:41:34 +0900

    refactor: MentoringErrorCode 정리

--- src/main/java/com/careerfit/mentoring/exception/MentoringErrorCode.java
+++ src/main/java/com/careerfit/mentoring/exception/MentoringErrorCode.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum MentoringErrorCode implements ErrorCode {
-    MENTORING_NOT_FOUND(HttpStatus.NOT_FOUND, "MENTORING-001", "멘토링을 찾을 수 없습니다."),
-    DOCUMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "DOCUMENT-001", "해당 문서가 없습니다."),
-    UNAUTHORIZED_DELETE(HttpStatus.FORBIDDEN, "AUTH-001", "삭제 권한이 없습니다.");
-
+    MENTORING_NOT_FOUND(HttpStatus.NOT_FOUND, "MENTORING-001", "멘토링을 찾을 수 없습니다.");
     private final HttpStatus status;
     private final String code;
     private final String message;
