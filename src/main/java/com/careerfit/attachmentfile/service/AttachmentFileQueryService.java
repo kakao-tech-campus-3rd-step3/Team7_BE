8645a1061b7c1b50b31679aa20104b3f55abc75a
Hyun <hyun_7586@naver.com>
2025-09-29 16:16:53 +0900

    feat: AttachmentQueryService 리스트 조회 메서드에 페이지네이션 적용

--- src/main/java/com/careerfit/attachmentfile/service/AttachmentFileQueryService.java
+++ src/main/java/com/careerfit/attachmentfile/service/AttachmentFileQueryService.java
@@ -4,14 +4,15 @@ import com.careerfit.attachmentfile.domain.AttachmentFile;
 import com.careerfit.attachmentfile.dto.FileInfoResponse;
 import com.careerfit.attachmentfile.repository.AttachmentFileRepository;
 import com.careerfit.document.domain.DocumentType;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly=true)
+@Transactional(readOnly = true)
 public class AttachmentFileQueryService {
 
     private final AttachmentFileRepository attachmentFileRepository;
@@ -27,11 +28,10 @@ public class AttachmentFileQueryService {
     }
 
     // 리스트 조회
-    public List<FileInfoResponse> getFileInfoList(Long applicationId, DocumentType documentType) {
-        return attachmentFileRepository.findAllByApplicationId(applicationId)
-            .stream()
-            .filter(each -> each.getAttachmentFileType().equals(documentType))
-            .map(FileInfoResponse::fromAttachmentFile)
-            .toList();
+    public Page<FileInfoResponse> getFileInfoList(Long applicationId, DocumentType documentType,
+        Pageable pageable) {
+        return attachmentFileRepository.findAllByApplicationIdAndAttachmentFileType(applicationId,
+                documentType, pageable)
+            .map(FileInfoResponse::fromAttachmentFile);
     }
 }
