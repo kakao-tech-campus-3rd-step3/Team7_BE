cf0abf1aa12497a9bc73a0232a19459693801593
lwj0831 <wjl0831@gmail.com>
2025-10-01 22:25:15 +0900

    chore(coverLetter): 삭제한 CoverLetterDetailResponse 다시 추가

---
+++ src/main/java/com/careerfit/coverletter/dto/CoverLetterDetailResponse.java
@@ -0,0 +1,20 @@
+package com.careerfit.coverletter.dto;
+
+import java.util.List;
+
+import com.careerfit.coverletter.domain.CoverLetterItem;
+
+public record CoverLetterDetailResponse(
+    String title,
+    List<CoverLetterItemInfoResponse> coverLetterItems
+) {
+
+    public static CoverLetterDetailResponse of(String title,
+        List<CoverLetterItem> coverLetterItems) {
+        List<CoverLetterItemInfoResponse> coverLetterItemInfoResponses =
+            coverLetterItems.stream()
+                .map(CoverLetterItemInfoResponse::from)
+                .toList();
+        return new CoverLetterDetailResponse(title, coverLetterItemInfoResponses);
+    }
+}
