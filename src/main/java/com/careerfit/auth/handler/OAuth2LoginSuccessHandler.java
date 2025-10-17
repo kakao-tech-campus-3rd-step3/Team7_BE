8dfcea952f87b95143c499557ce39de0ca88c0f5
lwj0831 <wjl0831@gmail.com>
2025-10-17 21:01:17 +0900

    chore(auth): 파라미터 네이밍 수정

--- src/main/java/com/careerfit/auth/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/careerfit/auth/handler/OAuth2LoginSuccessHandler.java
@@ -1,38 +1,38 @@
 package com.careerfit.auth.handler;
 
+import java.util.Map;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
 import com.careerfit.auth.domain.CustomOAuth2User;
 import com.careerfit.auth.domain.OAuthProvider;
 import com.careerfit.auth.dto.LoginResponse;
 import com.careerfit.auth.dto.OAuthUserInfo;
 import com.careerfit.auth.dto.TokenInfo;
 import com.careerfit.auth.service.AuthService;
-import com.careerfit.auth.utils.JwtProvider;
 import com.careerfit.global.dto.ApiResponse;
 import com.careerfit.member.domain.Member;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
-
 import java.io.IOException;
-import java.util.Map;
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private final JwtProvider jwtProvider;
     private final ObjectMapper objectMapper;
     private final AuthService authService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
+        Authentication authentication) throws IOException, ServletException {
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
         Map<String, Object> attributes = oAuth2User.getAttributes();
