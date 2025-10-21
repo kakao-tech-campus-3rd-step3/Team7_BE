0d0e6048816e2ca506850b5e8293fe331978e301
Hyun <hyun_7586@naver.com>
2025-10-21 16:14:52 +0900

    refactor: CommentUpdateRequest, CommentCommandService에 page 컬럼 추가 변경사항 반영
    - CommentUpdateRequest 필드들에 존재하던 제약조건 삭제
    - 이제 변경하고 싶은 필드값에 null이 아닌 값을 전달할 때만 update되도록 수정

--- src/main/java/com/careerfit/comment/dto/CommentUpdateRequest.java
+++ src/main/java/com/careerfit/comment/dto/CommentUpdateRequest.java
@@ -1,9 +1,8 @@
 package com.careerfit.comment.dto;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record CommentUpdateRequest(
-    @NotBlank(message = "댓글 내용은 비워둘 수 없습니다.")
-    String content
+    String content,
+    Integer page
 ) {
+
 }
