564f6b3a7478f1e7dab94dc21ba544f377a9ce9b
Hyun <hyun_7586@naver.com>
2025-11-06 16:57:48 +0900

    docs: 멘토 대시보드 조회 API swagger 작업

--- src/main/java/com/careerfit/member/controller/MentorQueryControllerDocs.java
+++ src/main/java/com/careerfit/member/controller/MentorQueryControllerDocs.java
@@ -1,12 +1,14 @@
 package com.careerfit.member.controller;
 
 import com.careerfit.global.dto.ApiResponse;
+import com.careerfit.member.dto.mentor.MentorDashboardItem;
 import com.careerfit.member.dto.mentor.MentorHeaderResponse;
 import com.careerfit.member.dto.mentor.MentorIntroductionResponse;
 import com.careerfit.member.dto.mentor.MentorListPageResponse;
 import com.careerfit.member.dto.mentor.MentorReviewResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,6 +38,12 @@ public interface MentorQueryControllerDocs {
 
     @Operation(summary = "멘토 리뷰 조회", description = "멘토 ID로 리뷰 정보를 조회합니다.")
     ResponseEntity<ApiResponse<MentorReviewResponse>> getMentorReviews(
+        @PathVariable Long mentorId
+    );
+
+    @Operation(summary = "멘토 대시보드 조회", description = "멘토가 받은 멘토링 요청 또는 남긴 코멘트와 관련된 문서 목록을 조회합니다. "
+        + "멘토가 남긴 코멘트의 생성 시각 or 멘토링 요청 시각을 기준으로 정렬됩니다.")
+    ResponseEntity<ApiResponse<List<MentorDashboardItem>>> getMentorDashboard(
         @PathVariable Long mentorId
     );
 }
